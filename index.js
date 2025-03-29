cf922ca5c265a98328ec997f5f2701c30bc1afe6
Md. Samiul Islam Soumik <soumik.shu@gmail.com>
2025-03-29 22:24:46 +0600

    Connect MongDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 3000
 
@@ -9,11 +10,39 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 app.get('/', (req, res) => {
   res.send(`You are in Champion's Choice Server`)
 })
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const database = client.db('sportsEquipmentStoreDB');
+    const equipmentsCollection = database.collection('equipments');
+    const usersCollection = database.collection('users');
+
+    
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //   await client.close();
+  }
+}
 
 app.listen(port, () => {
   console.log(`Champion's Choice Server is running in port: ${port}`);
